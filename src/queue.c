58fa862abd46af11781c4606de6b5614e79a9fed
Nhi Nguyễn Ngô Uyên <nhi.nguyenngouyen@gmail.com>
2025-04-23 20:03:30 +0700

    Initial commit

---
+++ src/queue.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t * q) {
+        if (q == NULL) return 1;
+	return (q->size == 0);
+}
+
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+        /* TODO: put a new process to queue [q] */
+        if (q->size < MAX_QUEUE_SIZE) { 
+                q->proc[q->size++] = proc; 
+        }
+}
+
+struct pcb_t * dequeue(struct queue_t * q) {
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to remove it from q
+         * */
+        if (empty(q)) return NULL;
+
+        #ifdef MLQ_SCHED
+            struct pcb_t *ret = q->proc[0];
+            for (int i = 1; i < q->size; ++i) {
+                q->proc[i - 1] = q->proc[i];
+            }
+            q->size--;
+            return ret;
+        #else
+            int highest_prio_index = 0;
+            for (int i = 1; i < q->size; ++i) {
+                if (q->proc[i]->priority < q->proc[highest_prio_index]->priority) {
+                    highest_prio_index = i;
+                }
+            }
+        
+            struct pcb_t *ret = q->proc[highest_prio_index];
+            for (int i = highest_prio_index; i < q->size - 1; ++i) {
+                q->proc[i] = q->proc[i + 1];
+            }
+            q->size--;
+            return ret;
+        #endif
+}
+
